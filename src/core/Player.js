a8b51f4256282b9bf37dd1b5065e939432ac32cd
Matheus Heinzen <matheusheinzen7@gmail.com>
2025-05-04 22:02:55 -0300

    Primeira versão do player

--- src/core/Player.js
+++ src/core/Player.js
@@ -36,9 +36,74 @@ export class Player {
   }
 
   draw() {
-    this.p.fill(200, 100, 255);
-    this.p.ellipse(this.pos.x, this.pos.y, 30, 30); 
+    this.drawPlayer(this.pos.x, this.pos.y);
   }
+
+  drawPlayer(x, y) {
+    //Sombra
+    this.p.noStroke();
+    this.p.fill(0, 60);
+    this.p.ellipse(x, y + 30, 15 * 1.6, 10);
+  
+    //Capacete
+    this.p.fill(50, 50, 100);
+    this.p.rect(x - 10, y - 32, 20, 22, 5); //base
+    this.p.fill(0, 200, 255);
+    this.p.rect(x - 6, y - 26, 12, 6); //visor principal
+  
+    this.p.fill(255); 
+    this.p.rect(x - 2, y - 26, 2, 5); //reflexo no visor
+  
+    //Antena no capacete
+    this.p.stroke(100, 200, 255);
+    this.p.strokeWeight(2);
+    this.p.line(x + 8, y - 32, x + 8, y - 42);
+    this.p.noStroke();
+  
+    //Ombreiras
+    this.p.fill(90, 90, 130);
+    this.p.rect(x - 13, y - 10, 6, 10); //esquerda
+    this.p.rect(x + 7, y - 10, 6, 10); //direita
+  
+    //Peitoral
+    this.p.fill(50, 50, 110);
+    this.p.rect(x - 9, y - 10, 18, 22, 3);
+  
+    //Detalhes do peito
+    this.p.fill(255, 100, 0);
+    this.p.rect(x - 3, y, 2, 3);
+    this.p.fill(0, 255, 100);
+    this.p.rect(x + 2, y, 2, 3);
+  
+    //Cinto
+    this.p.fill(60, 30, 0);
+    this.p.rect(x - 10, y + 8, 20, 4);
+    this.p.fill(255, 180, 0);
+    for (let i = -8; i <= 6; i += 4) {
+      this.p.rect(x + i, y + 8, 2, 4);
+    }
+  
+    //Braços
+    this.p.fill(50, 50, 100);
+    this.p.rect(x - 15, y , 4, 10); //esquerdo
+    this.p.rect(x + 11, y , 4, 10); //direito
+  
+    //Luvas
+    this.p.fill(0);
+    this.p.rect(x - 15, y + 10, 4, 3);
+    this.p.rect(x + 11, y + 10, 4, 3);
+  
+    //Pernas
+    this.p.fill(70, 70, 140);
+    this.p.rect(x - 7, y + 13, 6, 14); //esquerda
+    this.p.rect(x + 1, y + 13, 6, 14); //direita
+  
+    //Botas
+    this.p.fill(30);
+    this.p.rect(x - 7, y + 26, 6, 4);
+    this.p.rect(x + 1, y + 26, 6, 4);
+  };
+  
 
   jump() {
     console.log("Tentando pular. onGround:", this.onGround);
